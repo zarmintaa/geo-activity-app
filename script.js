08e3bea905ecaa15f50ef4ac18032bc222fd44b1
zarminta <18102217@ittelkom-pwt.ac.id>
2021-08-30 09:24:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,16 +16,31 @@ const successMap = (position) => {
   const { latitude } = position.coords;
   const { longitude } = position.coords;
   console.log(`https://www.google.co.id/maps/@${latitude},${longitude}`);
-  
-  const map = L.map('map').setView([51.505, -0.09], 13);
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  const coords = [latitude, longitude]
+  const map = L.map('map').setView(coords, 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
-  L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+  // L.marker(coords).addTo(map)
+  //   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+  //   .openPopup();
+
+  map.on('click', function(mapEvent) {
+    const {lat, lng} = mapEvent.latlng
+    L.marker([lat, lng]).addTo(map)
+      .bindPopup(L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup'
+      }))
+      .setPopupContent('Workout')
+      .openPopup();
+  })
 }
 
 const failedGetMap = () => {
